4eeecb02e555e9278e6699df967ffd656aec27de
yvesbahokenstudely <yves.bahoken@studely.com>
2026-05-04 15:32:40 +0100

    fix : change env values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://api-purplepoint-dev.up.railway.app/api/v1',
   cognito: {
     userPoolId: 'us-east-1_U3yCwN5yQ',
     clientId: '3nngurnl4v1m22l4l21r7oivph',
